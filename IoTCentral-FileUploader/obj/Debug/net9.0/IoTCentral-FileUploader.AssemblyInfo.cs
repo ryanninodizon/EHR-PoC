63ae98136c38ad5d32bb7c778617b8b893c973ab
Ryan Niño Dizon <ryanninodizon@gmail.com>
2025-02-03 13:45:07 +0100

    added git ignore and removed unnecessary files and folders

--- IoTCentral-FileUploader/obj/Debug/net9.0/IoTCentral-FileUploader.AssemblyInfo.cs
+++ IoTCentral-FileUploader/obj/Debug/net9.0/IoTCentral-FileUploader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IoTCentral-FileUploader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ea36c34234e6b53e33b9b1dc56e756800150c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+701c61a3d9c14e3c5509601ca30f2050ec3fab8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("IoTCentral-FileUploader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IoTCentral-FileUploader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
